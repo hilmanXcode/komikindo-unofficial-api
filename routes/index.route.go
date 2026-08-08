841b6ff6f3611bb6691108329fc30e213dfe9da1
hilmanXcode <hilmanxcode@gmail.com>
2026-08-08 20:58:04 +0700

    using cord

--- routes/index.route.go
+++ routes/index.route.go
@@ -3,7 +3,9 @@ package routes
 import (
 	"komikindo-scraper/controllers"
 	"komikindo-scraper/middleware"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
@@ -13,6 +15,14 @@ func InitRoute(app *gin.Engine, db *gorm.DB) {
 	komikindoController := controllers.NewKomikindoController(db)
 
 	route := app
+
+	route.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://my-app-navy-kappa-j7tvzh852l.vercel.app"},
+		AllowMethods:     []string{"GET"},
+		AllowHeaders:     []string{"Authorization"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	route.Use(middleware.RateLimiter(2, 5))
 	route.Use(middleware.RequireApiKey())
